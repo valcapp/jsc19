086d5b0ca07c5c2a73cc5f984ba777cd334edd89
Valerio Cappuccio <valerio.cappuccio@lloydsbanking.com>
2020-05-21 16:59:32 +0100

    some minor changes about diagrams

--- generate/diagram.js
+++ generate/diagram.js
@@ -4,19 +4,21 @@ const fs = require('fs'),
     diagramHref = '/img/diagram.png',
     diagramPath = path.join(appDir,'public',diagramHref),
     sketchHref = '/img/sketch.png',
-    sketchPath = path.join(appDir,'public',sketchHref),
-    checkPath = function(filePath,successCallback){
-        fs.access(filePath, (err) => {
-            if (err) {
-                console.error(err);
-            }else{
-                successCallback();
-            }
-        });
-    };
+    sketchPath = path.join(appDir,'public',sketchHref);
+// const checkPath = function(filePath,successCallback){
+//         fs.access(filePath, (err) => {
+//             if (err) {
+//                 console.error(err);
+//             }else{
+//                 successCallback();
+//             }
+//         });
+//     };
+// dashbDiagram = false;
+// checkPath(sketchPath, ()=> {dashbDiagram = sketchHref;});
+// checkPath(diagramPath, ()=> {dashbDiagram = diagramHref;});
 
-dashbDiagram = false;
-checkPath(sketchPath, ()=> {dashbDiagram = sketchHref;});
-checkPath(diagramPath, ()=> {dashbDiagram = diagramHref;});
-
+dashbDiagram = fs.existsSync(sketchPath)? sketchHref :
+    fs.existsSync(diagramPath)? diagramHref :
+    false;
 

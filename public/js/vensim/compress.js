1994f6a01b68075e8dfb46c699832fb79e5267ef
Valerio Cappuccio <valerio.cappuccio@lloydsbanking.com>
2020-05-27 08:51:03 +0100

    fixed position of sidebar

--- public/js/vensim/compress.js
+++ public/js/vensim/compress.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     $('#sidebarCollapse').on('click', function () {
         $('#sideBar').toggleClass('compressed');
+        $('#contentDiv').toggleClass('compressed');
     });
 
 });
